0daa20fa21223e7f7764f547cc4e8fe21a4131e6
henriquekiki <ricckdmo@gmail.com>
2023-06-27 14:53:11 -0300

    Começando a ficar doido.

--- banco-lh/src/SimuladorBanco.java
+++ banco-lh/src/SimuladorBanco.java
@@ -1,5 +1,15 @@
 public class SimuladorBanco {
     public static void main(String[] args) {
+        Cliente cliente = new Cliente("Luiz Lopes", "Rua aires da cunha, 0", "11 96913-0686");
+        Cliente cliente1 = new Cliente("Ariani Lopes", "Rua aires da cunha, 0", "11 96913-0686");
+
+        Banco banco = new Banco();
+
+        banco.abrirConta(1, cliente);
+        banco.abrirConta(2, cliente1);
         
+        banco.listarContas();
+
+
     }
 }
