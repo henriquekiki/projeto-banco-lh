5ecd7684331f42fb96451ffe98966f5162a4ce35
henriquekiki <ricckdmo@gmail.com>
2023-06-27 15:16:19 -0300

    Que bagunça.

--- banco-lh/src/SimuladorBanco.java
+++ banco-lh/src/SimuladorBanco.java
@@ -1,15 +1,25 @@
 public class SimuladorBanco {
     public static void main(String[] args) {
-        Cliente cliente = new Cliente("Luiz Lopes", "Rua aires da cunha, 0", "11 96913-0686");
-        Cliente cliente1 = new Cliente("Ariani Lopes", "Rua aires da cunha, 0", "11 96913-0686");
+        
+        Cliente cliente = new Cliente("Luiz Lopes", "Rua aires da cunha", "(11) 96913-0686");
 
+        
         Banco banco = new Banco();
 
-        banco.abrirConta(1, cliente);
-        banco.abrirConta(2, cliente1);
         
-        banco.listarContas();
+        banco.abrirConta(1, cliente);
 
+        
+        int numeroConta = 1;
+        double valorDeposito = 100.0;
 
+        ContaBancaria conta = banco.buscarConta(numeroConta);
+        if (conta != null) {
+            conta.depositar(valorDeposito);
+            System.out.println("Depósito realizado com sucesso na conta " + numeroConta);
+            System.out.println("Saldo atual: " + conta.consultarSaldo());
+        } else {
+            System.out.println("Conta não encontrada.");
+        }
     }
 }
